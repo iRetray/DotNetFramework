7f69991c741952f99d5cfd137280b0e0df87adde
Julian Camilo Cruz <julian20141016@gmail.com>
2021-08-09 22:02:08 -0500

    Creada carpeta para Fabric UI local

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,52 +1,21 @@
-var valorNumerico = 100;
-var estaCasado = true;
-var nombrePersona = "Jaime Alberto";
-var inicial = "J";
-var precioProducto = 5000.34;
-
-function cambiarContenido() {
-  document.getElementById("etiquetaValor").innerHTML = valorNumerico;
-  document.getElementById("etiquetaBooleano").innerHTML = estaCasado;
-  document.getElementById("etiquetaPersona").innerHTML = nombrePersona;
-  document.getElementById("etiquetaInicial").innerHTML = inicial;
-  document.getElementById("etiquetaPrecio").innerHTML = precioProducto;
+function addNumbers(number1, number2) {
+  return parseInt(number1) + parseInt(number2);
 }
 
-function sumarNumeros(valor1, valor2) {
-  var resultado = 0;
+const inputNumber1 = document.querySelector("#inputNumber1");
+inputNumber1.addEventListener("input", (event) => {
+  const inputNumber2 = document.querySelector("#inputNumber2");
+  const buttonFinal = document.querySelector("#buttonFinal");
+  buttonFinal.innerHTML =
+    "El resultado de la suma es: " +
+    addNumbers(event.target.value, inputNumber2 ? inputNumber2.value : 0);
+});
 
-  resultado = valor1 + valor2;
-
-  return resultado;
-}
-
-function mostrarCalculo() {
-  document.getElementById("resultadoSuma").innerHTML = sumarNumeros(4, 9);
-}
-
-function mostrarCalculo2() {
-  document.getElementById("resultadoSuma2").innerHTML = sumarNumeros(
-    "Clase NET",
-    " Viernes"
-  );
-}
-
-function calcularSumaDatos() {
-  var n1 = 0;
-  var n2 = 0;
-  var resultado = 0;
-
-  n1 = parseInt(prompt("Digite el primer valor"));
-  n2 = parseInt(prompt("Digite el segundo valor"));
-
-  resultado = sumarNumeros(n1, n2);
-
-  alert(
-    "El resultado de la suma de N1 = " +
-      n1 +
-      " y N2 = " +
-      n2 +
-      " es igual a = " +
-      resultado
-  );
-}
+const inputNumber2 = document.querySelector("#inputNumber2");
+inputNumber2.addEventListener("input", (event) => {
+  const inputNumber1 = document.querySelector("#inputNumber1");
+  const buttonFinal = document.querySelector("#buttonFinal");
+  buttonFinal.innerHTML =
+    "El resultado de la suma es: " +
+    addNumbers(event.target.value, inputNumber1 ? inputNumber1.value : 0);
+});
